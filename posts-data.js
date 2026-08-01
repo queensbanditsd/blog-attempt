de4c667b203cab945e38ec21cf7f7bc6f9f50fa1
queensbanditsd <queensbanditsd@gmail.com>
2026-08-01 12:37:18 +0530

    Update posts-data.js

--- posts-data.js
+++ posts-data.js
@@ -10,21 +10,21 @@ const POSTS = [
     date: "2026-08-01",
     title: "First entry",
     body: [
-"This started because I need an outlet and I wanted to prove to myself that I can make a website if I damn well want to."
-"NEET-PG is on August 30. Today's August 1st. I'm not perfectly prepared. I'm not even well-prepared. If I had to put a number to it, I'm at 50% of required preparation. But I'm sure as I get to 60%, 100% will just get farther away. Such is the nature of preparation."
-"Since I can't do much about time wasted, the only thing I can control is working smart now. For the last 30 days of preparation, that means solving as many MCQs as possible. As MANY as POSSIBLE."
-"Here's the plan:"
-"Morning"
-"50 MCQ custom modules, random, no tags, mixed subjects. I'll just deal with whatever marrow throws at me. (Great way to wake up, huh?)"
-"6hrs of major subject focused MCQ solving +reading."
-"3 hrs of minor subject focused MCQ+notes"
-"To end the day, 50 mixed subject PYQs."
-"And somehow in the middle of all this, I'm going to get through the Anki cards I've managed to somehow mature."
-"I've always considered myself a rather weak personality. And the way my maternal/grand maternal figures treat me have reinforced this in the past. Someone to whom discipline doesn't come easily. Someone who never puts her best effort. Someone who stays mediocre and doesn't have achievements worth bragging much about."
-"What is achievement anyway? If it's a <5k rank in NEET, I'm sure as hell not making it. This time, at least. But, something in me is so restless. 29 days now. Could I just set myself a challenge to stick to this plan? To stick to something for once? Regardless of the outcome?"
-"Hence this blog."
-"I'll document my daily wins/losses/thoughts on here."
-"Some might think it's a waste of time. But no, it might be the only thing keeping me true to myself."
+      "This started because I need an outlet and I wanted to prove to myself that I can make a website if I damn well want to."
+      "NEET-PG is on August 30. Today's August 1st. I'm not perfectly prepared. I'm not even well-prepared. If I had to put a number to it, I'm at 50% of required preparation. But I'm sure as I get to 60%, 100% will just get farther away. Such is the nature of preparation."
+      "Since I can't do much about time wasted, the only thing I can control is working smart now. For the last 30 days of preparation, that means solving as many MCQs as possible. As MANY as POSSIBLE."
+      "Here's the plan:"
+      "Morning"
+      "50 MCQ custom modules, random, no tags, mixed subjects. I'll just deal with whatever marrow throws at me. (Great way to wake up, huh?)"
+      "6hrs of major subject focused MCQ solving +reading."
+      "3 hrs of minor subject focused MCQ+notes"
+      "To end the day, 50 mixed subject PYQs."
+      "And somehow in the middle of all this, I'm going to get through the Anki cards I've managed to somehow mature."
+      "I've always considered myself a rather weak personality. And the way my maternal/grand maternal figures treat me have reinforced this in the past. Someone to whom discipline doesn't come easily. Someone who never puts her best effort. Someone who stays mediocre and doesn't have achievements worth bragging much about."
+      "What is achievement anyway? If it's a <5k rank in NEET, I'm sure as hell not making it. This time, at least. But, something in me is so restless. 29 days now. Could I just set myself a challenge to stick to this plan? To stick to something for once? Regardless of the outcome?"
+      "Hence this blog."
+      "I'll document my daily wins/losses/thoughts on here."
+      "Some might think it's a waste of time. But no, it might be the only thing keeping me true to myself."
       ,
     ],
   },
