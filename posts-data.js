5f9e4926f4be83bdda408bb07d3109143b702f7d
queensbanditsd <queensbanditsd@gmail.com>
2026-08-02 10:25:26 +0530

    Update posts-data.js

--- posts-data.js
+++ posts-data.js
@@ -6,6 +6,15 @@
 // body:  an array of paragraphs (usually you'll just need one)
 
 const POSTS = [
+  { 
+    date: "2026-08-02",
+    title: "Humbled by MCQs",
+    body: [
+      "There are many ways to get an MCQ wrong. Didn't know it, knew it but blanked, confused it with something else, misread the question.",
+      "It's been humbling to look at my incorrects from GTs and realise a not-insignificant number of them were because I misread the question. It's amazing how fickle and oblivious my brain can be. I could be absolutely sure I understood the question, mark my answer, DOUBLE CHECK while doing my 2nd pass and still have misread a word in the question.",
+      "I'm in the 100-110 corrects range, sometimes worse. Not at all a good spot to be in 1 month before NEET-PG. And the Marrow National Mock is next week. At this point, I'm just studying with no expectation. Let whatever happens happen. The result can be whatever. I'm just going to study and enjoy it. Well, maybe enjoy is an overstatement. I'll grin and bear it.",
+    ],
+  },
   {
     date: "2026-08-01",
     title: "First entry",
@@ -15,10 +24,10 @@ const POSTS = [
       "Since I can't do much about time wasted, the only thing I can control is working smart now. For the last 30 days of preparation, that means solving as many MCQs as possible. As MANY as POSSIBLE.",
       "Here's the plan:",
       "Morning",
-      "50 MCQ custom modules, random, no tags, mixed subjects. I'll just deal with whatever marrow throws at me. (Great way to wake up, huh?)",
-      "6hrs of major subject focused MCQ solving +reading.",
+      "36 MCQ custom modules, random, no tags, mixed subjects. I'll just deal with whatever marrow throws at me. (Great way to wake up, huh?)",
+      "6 hrs of major subject focused MCQ solving +reading.",
       "3 hrs of minor subject focused MCQ+notes",
-      "To end the day, 50 mixed subject PYQs.",
+      "To end the day, 36 mixed subject PYQs.",
       "And somehow in the middle of all this, I'm going to get through the Anki cards I've managed to somehow mature.",
       "I've always considered myself a rather weak personality. And the way my maternal/grand maternal figures treat me have reinforced this in the past. Someone to whom discipline doesn't come easily. Someone who never puts her best effort. Someone who stays mediocre and doesn't have achievements worth bragging much about.",
       "What is achievement anyway? If it's a <5k rank in NEET, I'm sure as hell not making it. This time, at least. But, something in me is so restless. 29 days now. Could I just set myself a challenge to stick to this plan? To stick to something for once? Regardless of the outcome?",
